acd757c3251b00561d3bb198cd7077ba7bae87b9
Sergey Korneev <korneevsk.work@gmail.com>
2020-06-26 23:50:45 +0300

    Added "Play" button that starts the game

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,6 +4,7 @@ from ship import Ship
 from game_stats import GameStats
 import game_functions as gf
 from pygame.sprite import Group
+from button import Button
 
 def run_game():
     # Инициализация игры и создание объекта экрана
@@ -11,6 +12,7 @@ def run_game():
     ai_settings = Settings()
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
+    play_button = Button(ai_settings, screen, "Play")
     stats = GameStats(ai_settings)
     # Создание корабля
     ship = Ship(ai_settings, screen)
@@ -22,12 +24,12 @@ def run_game():
 
     # Запуск основного цика игры
     while True:
-        gf.check_events(ai_settings, screen, ship, bullets)
+        gf.check_events(ai_settings, screen, stats, play_button, ship, bullets)
         if stats.game_active:
             ship.update()
             gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
             gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
-            gf.update_screen(ai_settings, screen, ship, aliens, bullets)
+        gf.update_screen(ai_settings, screen, stats, ship, aliens, bullets, play_button)
 
 
 run_game()
